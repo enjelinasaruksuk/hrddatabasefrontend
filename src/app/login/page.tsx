ecc1df42baa4cf518542247f5a62fe2dbd95f8c0
enjelinasaruksuk <enjelinasaruksuk@gmail.com>
2025-10-20 18:50:49 +0700

    Edit halaman login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -8,25 +8,19 @@ export default function LoginPage() {
   const router = useRouter();
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [role, setRole] = useState("hr"); 
+  const [role, setRole] = useState("hr");
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-
-    if (role === "hr") {
-      router.push("/hr");
-    } else if (role === "manajemen-it") {
-      router.push("/manajemen-it");
-    } else {
-      alert("Invalid role selected!");
-    }
-
-    console.log({ username, password, role });
+    if (role === "hr") router.push("/hr");
+    else if (role === "manajemen-it") router.push("/manajemen-it");
+    else alert("Invalid role selected!");
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gradient-to-b from-[#FFD54F] to-[#FFFFFF]">
-
+    <div
+      className="flex items-center justify-center min-h-screen bg-gradient-to-b from-[#FFD54F] to-[#FFFFFF] font-[Cambria]"
+    >
       <div className="flex bg-white shadow-md rounded-2xl p-8 w-[750px]">
         <div className="flex flex-col items-center justify-center w-1/2 pr-6 border-r">
           <Image
@@ -80,7 +74,7 @@ export default function LoginPage() {
 
             <button
               type="submit"
-              className="w-full bg-[#FDD835] text-black font-semibold py-2 rounded-lg shadow-lg hover:bg-[#FBC02D] transition-transform transform hover:scale-105"
+              className="w-full bg-[#FDD835] text-black font-semibold py-2 rounded-lg shadow-lg hover:bg-[#FBC02D] transition-transform transform hover:scale-105 font-[Cambria]"
             >
               LOGIN
             </button>
